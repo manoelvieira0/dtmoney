9512d95dbaa8da46e069ce4992028003e62253f0
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-03-11 05:49:05 -0300

    Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,31 @@ import { createServer, Model } from 'miragejs'
 import App from './App';
 
 createServer({
-
   models: {
     transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-03-09 14:55:12'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1200,
+          createdAt: new Date('2021-03-11 09:25:38'),
+        },
+      ]
+    })
   },
 
   routes(){
@@ -15,13 +37,13 @@ createServer({
     this.get('/transactions', () => {
       return this.schema.all('transaction')
     })
+
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody)
       return schema.create('transaction', data)
     })
   }
 })
-
 
 ReactDOM.render(
   <React.StrictMode>
